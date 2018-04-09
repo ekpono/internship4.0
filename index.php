02ebb5adf94211026e8a0246a27a6bd101aa984d
ekpono <ekponoambrose@gmail.com>
2018-04-09 07:23:08 -0800

    HND stage 1 task

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>First Task</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <div class="header">
+        <h1 style="line-height: 80px; color: #fff;">INTERSHIP</h1>
+        <span style="float: right; color: white"> Current time is     <?php echo date("h:i:sa"); ?> </span>
+    </div>
+        <div class="bodytext" style="color:#fff;"><span style="padding: 20px;">FIGMA SIMPLICITY</span>
+            <div class="text">
+                <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and scrambled it to make a 
+                         specimen book. It has survived not only five centuries, but also the leap 
+                          electronic typesetting, remaining essentially unchanged. It was popularised 
+                          in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                          and more recently with desktop publishing software like Aldus PageMaker 
+                          including versions of Lorem Ipsum.
+                </p>
+            </div>
+        </div>
+            <footer>
+                &copy copyright 2018
+            </footer>
+        
+
+</body>
+</html>
